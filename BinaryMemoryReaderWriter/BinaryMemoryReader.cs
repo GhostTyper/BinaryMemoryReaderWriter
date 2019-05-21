e0e0a252f46314096bc8882afc1f0332d406bc48
GhostTyper <matthias@lukaseder.de>
2019-05-21 04:50:35 +0200

    First Test-implementation with strings.

--- BinaryMemoryReaderWriter/BinaryMemoryReader.cs
+++ BinaryMemoryReaderWriter/BinaryMemoryReader.cs
@@ -14,5 +14,131 @@ namespace BinaryMemoryReaderWriter
             this.position = position;
             this.size = size;
         }
+
+        public int Size => size;
+
+        public string ReadString()
+        {
+            int length;
+            string result;
+
+            if (size <= 0)
+                throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+            if (*position == 0x00)
+            {
+                position++;
+                size--;
+
+                return null;
+            }
+
+            if ((*position & 0x80) == 0x00)
+            {
+                length = *position;
+
+                if (size < length + 1)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+                position++;
+                size--;
+
+                result = Encoding.UTF8.GetString(position, size);
+
+                position += length;
+                size -= length;
+
+                return result;
+            }
+
+            if (size < 2)
+                throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+            if (((*(position + 1)) & 0x80) == 0x00)
+            {
+                length = (*position & 0x7F) | ((*(position + 1) & 0x7F) << 7);
+
+                if (size < length + 2)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+                position += 2;
+                size -= 2;
+
+                result = Encoding.UTF8.GetString(position, size);
+
+                position += length;
+                size -= length;
+
+                return result;
+            }
+
+            if (size < 3)
+                throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+            if (((*(position + 2)) & 0x80) == 0x00)
+            {
+                length = (*position & 0x7F) | ((*(position + 1) & 0x7F) << 7) | ((*(position + 2) & 0x7F) << 14);
+
+                if (size < length + 3)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+                position += 3;
+                size -= 3;
+
+                result = Encoding.UTF8.GetString(position, size);
+
+                position += length;
+                size -= length;
+
+                return result;
+            }
+
+            if (size < 4)
+                throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+            if (((*(position + 3)) & 0x80) == 0x00)
+            {
+                length = (*position & 0x7F) | ((*(position + 1) & 0x7F) << 7) | ((*(position + 2) & 0x7F) << 14) | ((*(position + 3) & 0x7F) << 21);
+
+                if (size < length + 4)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+                position += 4;
+                size -= 4;
+
+                result = Encoding.UTF8.GetString(position, size);
+
+                position += length;
+                size -= length;
+
+                return result;
+            }
+
+            if (size < 5)
+                throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+            if (((*(position + 4)) & 0x80) == 0x00)
+            {
+                length = (*position & 0x7F) | ((*(position + 1) & 0x7F) << 7) | ((*(position + 2) & 0x7F) << 14) | ((*(position + 3) & 0x7F) << 21) | (((*(position + 4) & 0x7F) << 28) & 0x0F);
+
+                if (length < 0)
+                    throw new System.IO.InvalidDataException("Ambiguous length information.");
+
+                if (size < length + 5)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+
+                position += 5;
+                size -= 5;
+
+                result = Encoding.UTF8.GetString(position, size);
+
+                position += length;
+                size -= length;
+
+                return result;
+            }
+
+            throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryReader.");
+        }
     }
 }
--- BinaryMemoryReaderWriter/BinaryMemoryWriter.cs
+++ BinaryMemoryReaderWriter/BinaryMemoryWriter.cs
@@ -14,5 +14,82 @@ namespace BinaryMemoryReaderWriter
             this.position = position;
             this.size = size;
         }
+
+        public void Write(string text)
+        {
+            if (text == null)
+            {
+                if (size < 1)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryWriter.");
+
+                *(position++) = 0x00;
+                size--;
+
+                return;
+            }
+
+            int length = Encoding.UTF8.GetByteCount(text);
+
+            if (length < 128)
+            {
+                if (size < length + 1)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryWriter.");
+
+                *(position++) = (byte)length;
+                Encoding.UTF8.GetBytes(text.AsSpan(), new Span<byte>(position, size - 1));
+
+                size -= length + 1;
+            }
+            else if (length < 16384)
+            {
+                if (size < length + 2)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryWriter.");
+
+                *(position++) = (byte)(length | 0x80);
+                *(position++) = (byte)(length >> 7);
+                Encoding.UTF8.GetBytes(text.AsSpan(), new Span<byte>(position, size - 2));
+
+                size -= length + 2;
+            }
+            else if (length < 2097152)
+            {
+                if (size < length + 3)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryWriter.");
+
+                *(position++) = (byte)(length | 0x80);
+                *(position++) = (byte)((length >> 7) | 0x80);
+                *(position++) = (byte)(length >> 14);
+                Encoding.UTF8.GetBytes(text.AsSpan(), new Span<byte>(position, size - 3));
+
+                size -= length + 3;
+            }
+            else if (length < 268435456)
+            {
+                if (size < length + 4)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryWriter.");
+
+                *(position++) = (byte)(length | 0x80);
+                *(position++) = (byte)((length >> 7) | 0x80);
+                *(position++) = (byte)((length >> 14) | 0x80);
+                *(position++) = (byte)(length >> 21);
+                Encoding.UTF8.GetBytes(text.AsSpan(), new Span<byte>(position, size - 4));
+
+                size -= length + 4;
+            }
+            else
+            {
+                if (size < length + 5)
+                    throw new OutOfMemoryException("Not enough reserved memory for BinaryMemoryWriter.");
+
+                *(position++) = (byte)(length | 0x80);
+                *(position++) = (byte)((length >> 7) | 0x80);
+                *(position++) = (byte)((length >> 14) | 0x80);
+                *(position++) = (byte)((length >> 21) | 0x80);
+                *(position++) = (byte)(length >> 28);
+                Encoding.UTF8.GetBytes(text.AsSpan(), new Span<byte>(position, size - 5));
+
+                size -= length + 5;
+            }
+        }
     }
 }
